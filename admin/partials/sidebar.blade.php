439d54abb8f21195f7f2538b54645d4a6a90ba81
sahab uddin <sahabuddinriyaj984@gmail.com>
2019-09-08 15:17:30 +0600

    bootstarp dashboard convart in laravel blade template

---
+++ admin/partials/sidebar.blade.php
@@ -0,0 +1,21 @@
+<div class="app-sidebar__overlay" data-toggle="sidebar"></div>
+<aside class="app-sidebar">
+    <div class="app-sidebar__user">
+        <div>
+            <p class="app-sidebar__user-name">Sahab Uddin</p>
+            <p class="app-sidebar__user-designation">Frontend Developer</p>
+        </div>
+    </div>
+    <ul class="app-menu">
+        <li>
+            <a class="app-menu__item active" href="#"><i class="app-menu__icon fa fa-dashboard"></i>
+                <span class="app-menu__label">Dashboard</span>
+            </a>
+        </li>
+        <li>
+            <a class="app-menu__item" href="#"><i class="app-menu__icon fa fa-cogs"></i>
+                <span class="app-menu__label">Settings</span>
+            </a>
+        </li>
+    </ul>
+</aside>
